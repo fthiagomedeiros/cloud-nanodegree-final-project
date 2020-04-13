ce356aa15b2615d21a6f2c674dff5e10d5970eed
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-04-13 00:28:59 -0300

    feature: communication with dynamodb to insert new product

---
+++ products-service/src/businessLogic/product.ts
@@ -0,0 +1,17 @@
+import * as uuid from 'uuid'
+import {CreateProductRequest} from "../requests/CreateProductRequest";
+import {ProductAccess} from "../dataLayer/productsAccess";
+import {Product} from "../models/Product";
+
+const productAccess = new ProductAccess();
+
+export async function createProduct(
+    createProductRequest: CreateProductRequest): Promise<Product> {
+
+    return await productAccess.createProduct({
+        id: uuid.v4(),
+        name: createProductRequest.name,
+        description: createProductRequest.description,
+        price: createProductRequest.price
+    });
+}
--- products-service/src/lambda/createProduct.ts
+++ products-service/src/lambda/createProduct.ts
@@ -1,14 +1,16 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import { CreateProductRequest } from "../requests/CreateProductRequest";
+import { createProduct } from '../businessLogic/product'
 
 export const handler: APIGatewayProxyHandler = async (_event, _context) => {
 
   const newProduct: CreateProductRequest = JSON.parse(_event.body);
+  const product = await createProduct(newProduct);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(newProduct)
+    body: JSON.stringify(product)
   };
 
 };
