70c2bd6044cb942173ae6077847ef2db2592fabf
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-04-13 01:06:17 -0300

    feature: communication with dynamodb to insert new product

--- products-service/src/lambda/createProduct.ts
+++ products-service/src/lambda/createProduct.ts
@@ -1,16 +1,17 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import { CreateProductRequest } from "../requests/CreateProductRequest";
 import { createProduct } from '../businessLogic/product'
 
-export const handler: APIGatewayProxyHandler = async (_event, _context) => {
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { ApiResponse } from "../response/ApiResponse";
+
+export const handler = middy(async (_event, _context) => {
 
   const newProduct: CreateProductRequest = JSON.parse(_event.body);
   const product = await createProduct(newProduct);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(product)
-  };
+  return new ApiResponse().successResponse(200, 'item', product);
+});
 
-};
+handler.use(cors());
