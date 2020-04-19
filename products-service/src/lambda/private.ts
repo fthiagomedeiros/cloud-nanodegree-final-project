994dabc9a55436de459a542b6e96c738a65f723e
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-04-19 00:11:01 -0300

    feature: product id adjustments

--- products-service/src/lambda/private.ts
+++ products-service/src/lambda/private.ts
@@ -5,7 +5,7 @@ import { cors } from 'middy/middlewares'
 import {ApiResponse} from "../response/ApiResponse";
 
 export const handler = middy(async (_event, _context) => {
-    return new ApiResponse().message(200, '{"message": "Hello from a Private Api ar AWS ApiGateway"}');
+    return new ApiResponse().message(200, '{"message": "Hello from a PrivateApi at AWS ApiGateway"}');
 });
 
 handler.use(cors());
