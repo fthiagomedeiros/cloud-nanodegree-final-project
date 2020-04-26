6f8ca8b6769c33de30879f6b9b1f9141daed60a6
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-04-26 01:35:49 -0300

    Passing item clicked values to edit page

--- products-react-client/src/components/EditProduct.js
+++ products-react-client/src/components/EditProduct.js
@@ -2,18 +2,18 @@ import React, {Component} from 'react';
 
 class EditProduct extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     componentDidMount() {
 
     }
 
     render() {
+
         return (
             <div>
                 <h1>{this.props.match.params.id}</h1>
+                <p>{this.props.location.state.name}</p>
+                <p>{this.props.location.state.description}</p>
+                <p>{this.props.location.state.price}</p>
             </div>
         );
     }
