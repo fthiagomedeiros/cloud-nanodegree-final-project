406cebf1f9759be8ab66f27eb0255f7bcfcf7906
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-04-26 02:33:28 -0300

    editing product by id and updating attributes

--- products-react-client/src/components/EditProduct.js
+++ products-react-client/src/components/EditProduct.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
+import serializeForm from "form-serialize";
+import {postProduct} from "../api/ProductsApi";
 
 class EditProduct extends Component {
 
-    componentDidMount() {
-
+    state = {
+        id: this.props.match.params.id,
+        name: this.props.location.state.name,
+        description: this.props.location.state.description,
+        price: this.props.location.state.price
     }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    handleUpdate = (e) => {
+        e.preventDefault();
+        const values = serializeForm(e.target, { hash: true });
+        console.log(values);
+
+        postProduct({
+            name: this.state.name,
+            description: this.state.description,
+            price: this.state.price}, this.state.file, this.props.auth.getToken());
+
+    };
 
     render() {
 
         return (
             <div>
-                <h1>{this.props.match.params.id}</h1>
-                <p>{this.props.location.state.name}</p>
-                <p>{this.props.location.state.description}</p>
-                <p>{this.props.location.state.price}</p>
+                {this.state.id}
             </div>
         );
     }
